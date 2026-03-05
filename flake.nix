1ecbe99fb5deb933716b8d851b65dba60bccdff6
aytordev <me@aytor.dev>
2026-03-05 23:37:33 +0100

    :sparkles: feat(flake): scaffold flake entry point with flake-parts
    - Add flake.nix with 6 inputs (nixpkgs, flake-parts, nvf, home-manager,
      treefmt-nix, systems) and nixpkgs follows pinning
    - Add flake/default.nix as flake-parts root module with treefmt-nix
    - Add homes/ and packages/ directory placeholders

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "aytordev's Neovim distribution";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (import ./flake);
+}
