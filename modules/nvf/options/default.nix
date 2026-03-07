1965e80f46b86413b58f58a58e66e7dc85c4e418
aytordev <me@aytor.dev>
2026-03-07 18:19:21 +0100

    :sparkles: feat(editor): add opinionated editor options
    - Add tabWidth and clipboard as user-facing options
    - Bridge ~30 vim settings via nvf options (search, indent, display,
      scroll, timing, persistence)
    - Set nvf high-level options (searchCase, undoFile)
    - Add vim.globals for maplocalleader and markdown_recommended_style
    - Add shortmess:append via luaConfigRC DAG entry
    
    Consensus defaults from LazyVim, kickstart, AstroNvim, NvChad,
    and khanelivim comparison.

--- modules/nvf/options/default.nix
+++ modules/nvf/options/default.nix
@@ -1,6 +1,71 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   config.vim = {
     viAlias = config.aytordev.viAlias;
     vimAlias = config.aytordev.vimAlias;
+
+    searchCase = "smart";
+    undoFile.enable = true;
+
+    globals = {
+      maplocalleader = " ";
+      markdown_recommended_style = 0;
+    };
+
+    options =
+      {
+        autowrite = true;
+        confirm = true;
+        mouse = "a";
+        showmode = false;
+        formatoptions = "jcroqlnt";
+
+        tabstop = config.aytordev.tabWidth;
+        shiftwidth = config.aytordev.tabWidth;
+        shiftround = true;
+        smartindent = true;
+        copyindent = true;
+        breakindent = true;
+
+        cursorline = true;
+        laststatus = 3;
+        conceallevel = 2;
+        linebreak = true;
+        wrap = false;
+        pumblend = 10;
+        pumheight = 10;
+        fillchars = "eob: ";
+
+        inccommand = "split";
+        grepprg = "rg --vimgrep";
+        grepformat = "%f:%l:%c:%m";
+        infercase = true;
+
+        scrolloff = 8;
+        sidescrolloff = 8;
+        smoothscroll = true;
+        virtualedit = "block";
+        splitkeep = "screen";
+
+        timeoutlen = 300;
+        updatetime = 200;
+
+        undolevels = 10000;
+        sessionoptions = "buffers,curdir,tabpages,winsize,help,globals,skiprtp,folds";
+
+        winminwidth = 5;
+        wildmode = "longest:full,full";
+        spelllang = "en";
+      }
+      // lib.optionalAttrs (config.aytordev.clipboard != "none") {
+        clipboard = config.aytordev.clipboard;
+      };
+
+    luaConfigRC.editorShortmess = lib.nvim.dag.entryAfter ["optionsScript"] ''
+      vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+    '';
   };
 }
