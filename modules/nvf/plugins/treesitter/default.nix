49e03870ce84a947db7ea5b8547734ffdd324a1f
aytordev <me@aytor.dev>
2026-03-07 22:27:19 +0100

    :sparkles: feat(treesitter): add treesitter with context and textobjects
    Enable nvim-treesitter for syntax highlighting, treesitter-context for
    sticky function/class headers, and treesitter-textobjects for
    TS-based text object selection.

---
+++ modules/nvf/plugins/treesitter/default.nix
@@ -0,0 +1,9 @@
+# Treesitter syntax highlighting and code understanding
+# https://github.com/nvim-treesitter/nvim-treesitter
+{...}: {
+  config.vim.treesitter = {
+    enable = true;
+    context.enable = true;
+    textobjects.enable = true;
+  };
+}
