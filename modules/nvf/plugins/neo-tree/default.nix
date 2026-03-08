a5f7d69b4789cab01e81a673e0791004188001da
aytordev <me@aytor.dev>
2026-03-08 23:58:59 +0100

    :sparkles: feat(neo-tree): configure as right sidebar with close-on-last
    Set neo-tree to open as a 30-column right sidebar and
    auto-close when it's the last remaining window.

--- modules/nvf/plugins/neo-tree/default.nix
+++ modules/nvf/plugins/neo-tree/default.nix
@@ -1,5 +1,14 @@
 # Neo-tree file explorer
 # https://github.com/nvim-neo-tree/neo-tree.nvim
 {...}: {
-  config.vim.filetree.neo-tree.enable = true;
+  config.vim.filetree.neo-tree = {
+    enable = true;
+    setupOpts = {
+      window = {
+        position = "right";
+        width = 30;
+      };
+      close_if_last_window = true;
+    };
+  };
 }
